2ddabb13223b1f8433c8118dd4e259f4f86ddb8d
bitcamp <bitcamp@DESKTOP-KT7DNR2>
2022-04-15 09:51:38 +0900

    master : PurchaseController.java 코드 작성 - 2022/04/15 9:50
    실수로 master에서 작성해서 checkout을 위해 commit함
    addPurchaseView까지 작성

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/web/purchase/PurchaseController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
 import com.model2.mvc.service.purchase.PurchaseService;
 
 @Controller
@@ -14,6 +19,10 @@ public class PurchaseController {
 	@Autowired
 	@Qualifier("purchaseServiceImpl")
 	private PurchaseService purchaseService;
+	
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
 	
 	/// Constructor
 	public PurchaseController() {
@@ -26,4 +35,20 @@ public class PurchaseController {
 	@Value("#{commonProperties['pageSize']}")
 	int pageSize;
 
+	@RequestMapping("/addPurchaseView.do")
+	public ModelAndView addPurchaseView(@RequestParam("prodNo") int prodNo) throws Exception{
+		
+		System.out.println("/addPurchaseView.do");
+		
+		Product product = productService.getProduct(prodNo);
+		
+		System.out.println("addPurchaseView : " + product.getProdName());
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("forward:/purchase/addPurchaseView.jsp");
+		modelAndView.addObject("product", product);
+				
+		return modelAndView;
+	}
+	
 }//end of class
